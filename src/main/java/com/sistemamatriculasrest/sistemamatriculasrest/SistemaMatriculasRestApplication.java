45b3ee8b2444389a63b03f0232ea9b244052d587
y4zper <ander.crr@gmail.com>
2023-12-01 22:13:08 -0500

    a

---
+++ src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/SistemaMatriculasRestApplication.java
@@ -0,0 +1,13 @@
+package com.sistemamatriculasrest.sistemamatriculasrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMatriculasRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMatriculasRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemamatriculasrest/sistemamatriculasrest/SistemaMatriculasRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemamatriculasrest.sistemamatriculasrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMatriculasRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
