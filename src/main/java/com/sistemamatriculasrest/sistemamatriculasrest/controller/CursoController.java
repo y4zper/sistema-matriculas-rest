82237624f48fa6a557ac3bfebe0462dfafa2016c
Airthon1234 <cipoteland@gmail.com>
2023-12-02 01:22:58 -0500

    curso

--- src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/controller/CursoController.java
+++ src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/controller/CursoController.java
@@ -1,4 +1,11 @@
 package com.sistemamatriculasrest.sistemamatriculasrest.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/curso")
 public class CursoController {
 }
