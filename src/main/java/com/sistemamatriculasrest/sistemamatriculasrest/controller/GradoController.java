f827d1a9e4d05c9e09a3d5ef6bf1b9ed8b513d66
Airthon1234 <cipoteland@gmail.com>
2023-12-02 11:05:45 -0500

    grado servicio y controller

--- src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/controller/GradoController.java
+++ src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/controller/GradoController.java
@@ -1,4 +1,61 @@
 package com.sistemamatriculasrest.sistemamatriculasrest.controller;
 
+import com.sistemamatriculasrest.sistemamatriculasrest.exception.ResourceNotFoundException;
+import com.sistemamatriculasrest.sistemamatriculasrest.model.bd.Grado;
+import com.sistemamatriculasrest.sistemamatriculasrest.service.GradoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/grado")
 public class GradoController {
+
+    private GradoService gradoService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Grado>> listaGrados() {
+        List<Grado> gradoList = gradoService.listaGrados();
+        if (gradoList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(gradoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Grado> obtenerGradoPorId(@PathVariable("id") Integer id) {
+        Grado grado = gradoService.obtenerGradoPorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El grado con el ID " + id + " no existe"));
+        return new ResponseEntity<>(grado, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Grado> registrarGrado(@RequestBody Grado grado) {
+        Grado nuevoGrado = gradoService.registrarGrado(grado);
+        return new ResponseEntity<>(nuevoGrado, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Grado> actualizarGrado(@PathVariable("id") Integer id, @RequestBody Grado gradoActualizado) {
+        Grado grado = gradoService.actualizarGrado(id, gradoActualizado);
+        return new ResponseEntity<>(grado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Void> eliminarGrado(@PathVariable("id") Integer id) {
+        gradoService.eliminarGrado(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/nomgrado")
+    public ResponseEntity<List<Grado>> obtenerGradosPorNombre(@RequestParam String nomgrado) {
+        List<Grado> grados = gradoService.obtenerGradosPorNombre(nomgrado);
+        if (grados.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(grados, HttpStatus.OK);
+    }
 }
--- src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/service/GradoService.java
+++ src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/service/GradoService.java
@@ -1,4 +1,42 @@
 package com.sistemamatriculasrest.sistemamatriculasrest.service;
 
+import com.sistemamatriculasrest.sistemamatriculasrest.exception.ResourceNotFoundException;
+import com.sistemamatriculasrest.sistemamatriculasrest.model.bd.Grado;
+import com.sistemamatriculasrest.sistemamatriculasrest.repository.GradoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class GradoService {
+
+    private GradoRepository gradoRepository;
+
+    public List<Grado> listaGrados() {
+        return gradoRepository.findAll();
+    }
+
+    public Grado registrarGrado(Grado grado) {
+        return gradoRepository.save(grado);
+    }
+
+    public Optional<Grado> obtenerGradoPorId(Integer id) {
+        return gradoRepository.findById(id);
+    }
+
+    public void eliminarGrado(Integer id) {
+        gradoRepository.deleteById(id);
+    }
+
+    public List<Grado> obtenerGradosPorNombre(String nomgrado) {
+        return gradoRepository.findByNomgrado(nomgrado);
+    }
+
+    public Grado actualizarGrado(Integer id, Grado gradoActualizado) {
+        Grado oldGrado = obtenerGradoPorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El grado con el ID " + id + " no existe"));
+        oldGrado.setNomgrado(gradoActualizado.getNomgrado());
+        return gradoRepository.save(oldGrado);
+    }
 }
