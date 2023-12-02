e55210f1e2262d95d454601b1199491a4700880a
Airthon1234 <cipoteland@gmail.com>
2023-12-02 11:01:33 -0500

    docente servicio y controller

--- src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/controller/DocenteController.java
+++ src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/controller/DocenteController.java
@@ -1,4 +1,90 @@
 package com.sistemamatriculasrest.sistemamatriculasrest.controller;
 
+import com.sistemamatriculasrest.sistemamatriculasrest.exception.ResourceNotFoundException;
+import com.sistemamatriculasrest.sistemamatriculasrest.model.bd.Docente;
+import com.sistemamatriculasrest.sistemamatriculasrest.model.request.DocenteRequest;
+import com.sistemamatriculasrest.sistemamatriculasrest.service.DocenteService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/docente")
 public class DocenteController {
+
+    private DocenteService docenteService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Docente>> listaDocentes() {
+        List<Docente> docenteList = docenteService.listaDocentes();
+        if (docenteList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(docenteList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Docente> obtenerDocentePorId(@PathVariable("id") Integer id) {
+        Docente docente = docenteService.obtenerDocentePorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El docente con el ID " + id + " no existe"));
+        return new ResponseEntity<>(docente, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Docente> registrarDocente(@RequestBody DocenteRequest docenteRequest) {
+        Docente docente = convertirADocente(docenteRequest);
+        Docente nuevoDocente = docenteService.registrarDocente(docente);
+        return new ResponseEntity<>(nuevoDocente, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Docente> actualizarDocente(@PathVariable("id") Integer id, @RequestBody DocenteRequest docenteRequest) {
+        Docente oldDocente = docenteService.obtenerDocentePorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El docente con el ID " + id + " no existe"));
+        oldDocente.setNomdocente(docenteRequest.getNomdocente());
+        oldDocente.setEmail(docenteRequest.getEmail());
+        oldDocente.setActivo(docenteRequest.getActivo());
+        docenteService.registrarDocente(oldDocente);
+        return new ResponseEntity<>(oldDocente, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Void> eliminarDocente(@PathVariable("id") Integer id) {
+        Docente docente = docenteService.obtenerDocentePorId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El docente con el ID " + id + " no existe"));
+        docenteService.eliminarDocente(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/nombredocente")
+    public ResponseEntity<Docente> obtenerDocentePorNombre(@RequestParam String nomdocente) {
+        Docente docente = docenteService.obtenerDocentePorNombre(nomdocente);
+        if (docente == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(docente, HttpStatus.OK);
+    }
+
+    @GetMapping("/activos")
+    public ResponseEntity<List<Docente>> obtenerDocentesActivos() {
+        List<Docente> docentes = docenteService.obtenerDocentesActivos();
+        if (docentes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(docentes, HttpStatus.OK);
+    }
+
+    // Método para convertir DocenteRequest a Docente
+    private Docente convertirADocente(DocenteRequest docenteRequest) {
+        Docente docente = new Docente();
+        docente.setNomdocente(docenteRequest.getNomdocente());
+        docente.setEmail(docenteRequest.getEmail());
+        docente.setActivo(docenteRequest.getActivo());
+        docente.setFechacrea(docenteRequest.getFechacrea());
+        return docente;
+    }
 }
--- src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/service/DocenteService.java
+++ src/main/java/com/sistemamatriculasrest/sistemamatriculasrest/service/DocenteService.java
@@ -1,4 +1,40 @@
 package com.sistemamatriculasrest.sistemamatriculasrest.service;
 
+import com.sistemamatriculasrest.sistemamatriculasrest.model.bd.Docente;
+import com.sistemamatriculasrest.sistemamatriculasrest.repository.DocenteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DocenteService {
+
+    private DocenteRepository docenteRepository;
+
+    public List<Docente> listaDocentes() {
+        return docenteRepository.findAll();
+    }
+
+    public Docente registrarDocente(Docente docente) {
+        docente.setFechacrea(new Date());
+        return docenteRepository.save(docente);
+    }
+
+    public Optional<Docente> obtenerDocentePorId(Integer id) {
+        return docenteRepository.findById(id);
+    }
+
+    public void eliminarDocente(Integer id) {
+        docenteRepository.deleteById(id);
+    }
+
+    public Docente obtenerDocentePorNombre(String nomdocente) {
+        return docenteRepository.findByNomdocente(nomdocente);
+    }
+
+    public List<Docente> obtenerDocentesActivos() {
+        return docenteRepository.findByActivoTrue();
+    }
 }
